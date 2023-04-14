a47f3c5e2991b0be60b5bd2f9a8eaa17d7b56fd1
Nomad <yakushev_v@rambler.ru>
2023-04-14 14:51:00 +0300

    completed task 2

---
+++ Homework/Seminar_2/Task_2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 2: 
+//      Напишите программу, которая выводит третью цифру 
+//      заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+thirdNum(number);
+
+void printThirdNum(int number) {
+    if (number < 100) {
+        Console.Write("Третьей цифры нет.");
+    } else if (number >= 100 && number < 1000) {
+        number = number % 10;
+        Console.Write(number);
+    } else if (number >= 1000 && number < 10000) {
+        number = number % 100;
+        int num = number / 10;
+        Console.Write(num);
+    } else if (number >= 10000 && number < 100000) {
+        number = number % 1000;
+        int num = number / 100;
+        Console.Write(num);
+    } else {
+        Console.Write("Число слишком большое!");
+    }
+}
+
+
+
