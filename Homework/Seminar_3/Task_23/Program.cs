acd35fd4a594b10f12d2d112c02d80d4148ecfe7
Nomad <yakushev_v@rambler.ru>
2023-04-17 12:43:45 +0300

    completed Task_23

---
+++ Homework/Seminar_3/Task_23/Program.cs
@@ -0,0 +1,17 @@
+﻿//                  Задача 23.
+//  Напишите программу, которая принимает на вход число (N)
+//  и выдает таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// Решение без Math.Pow();
+// for (int i = 1; i <= number; i++) {
+//     int cube = i * i * i;
+//     Console.Write(cube + " ");
+// }
+
+// Решение с помощью Math.Pow()
+for (int i = 1; i <= number; i++) {
+    Console.Write(Math.Pow(i, 3) + " ");
+}
