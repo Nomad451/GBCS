5d082092e1ba087406f16b6fce2b83ba6366929c
Nomad <yakushev_v@rambler.ru>
2023-04-19 13:34:37 +0300

    add Task_26

---
+++ Seminars/Seminar_4/Task_26/Program.cs
@@ -0,0 +1,30 @@
+﻿//                  Задача 26.                     \\
+//  Напишите программу, которая принимает на вход  \\
+//  число и выдает количество цифр в числе.        \\
+//                                                 \\ 
+//  Пример:                                        \\ 
+//      456 -> 3                                   \\ 
+//       78 -> 2                                   \\ 
+//    89126 -> 5                                   \\ 
+//=================================================\\
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// 1 способ 
+int GetNumLength() {
+    return Convert.ToString(number).Length;
+}
+Console.Write("Способ 1: " + GetNumLength());
+Console.WriteLine();
+
+// 2 способ 
+int GetNumLength2() {
+    int i = 0;
+    while (number != 0) {
+        number /= 10;
+        i++; 
+    }
+    return i;
+}
+Console.Write("Способ 2: " + GetNumLength2());
