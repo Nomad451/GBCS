bbc77001525fe74b662b60fa91752897370eae95
Nomad <yakushev_v@rambler.ru>
2023-04-20 13:28:17 +0300

    completed Task_29

---
+++ Homework/Seminar_4/Task_29/Program.cs
@@ -0,0 +1,24 @@
+﻿//===========================================================\\
+//                  Задача 29.                               \\
+//  Напишите программу, которая задает массив из 8 элементов \\
+//  и выводит их на экран.                                   \\
+//                                                           \\
+//  Пример:                                                  \\
+//      1, 2, 5, 7, 19, 6, 1, 33 ->                          \\
+//     [1, 2, 5, 7, 19, 6, 1, 33]                            \\
+//===========================================================\\
+
+Console.Write("Укажите длину массива: ");
+int arrLength = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[arrLength];
+
+Console.WriteLine("Заполните и распечатайте массив: ");
+PrintArray(arr, arrLength);
+
+
+void PrintArray(int[] array, int arrayLength) {
+    for (int i = 0; i < arrayLength; i++) {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine("[" + String.Join(", ", array) + "]");
+}
